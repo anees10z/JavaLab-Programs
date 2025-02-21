902c6a619b22747d7fad0549f7f2e04a46f51378
Md Anees Khan <khanmdanis145@gmail.com>
2025-02-21 17:13:23 +0530

    Create SwapWith2Vars.java

---
+++ SwapWith2Vars.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SwapWith2Vars {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Swaping 2 numbers withouts using 3rd variable\n");
+    System.out.print("Enter 1st number: ");
+    int a = sc.nextInt();
+    System.out.print("Enter 2nd number: ");
+    int b = sc.nextInt();
+
+    // Display After swap
+    System.out.println("\nBefore Swap");
+    System.out.println("1st number =  " + a);
+    System.out.println("2nd number =  " + b);
+
+    // process 
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    // Display Before swap
+    System.out.println("\nAfter Swap");
+    System.out.println("1st number =  " + a);
+    System.out.println("2nd number =  " + b);
+  }
+}
